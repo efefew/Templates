7024ebc43a88c31c0971315e6a8be72a8004cab4
Роман Сёмин <prodigy19913@mail.ru>
2025-07-10 23:35:42 +0300

    Added new script for pausing game mechanics (PauseController.cs) - Issue #123 (main)

---
+++ Assets/Project/Templates/Scripts/Develop/Architecture/Pause/PauseController.cs
@@ -0,0 +1,59 @@
+#region
+
+using UnityEngine;
+using static PauseManager;
+
+#endregion
+
+public class PauseController : MonoBehaviour
+{
+
+    private void Update()
+    {
+        #if UNITY_EDITOR
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(IsPause)
+                Resume();
+            else
+                Pause();
+        }
+        #endif
+        if (IsPause)
+        {
+            return;
+        }
+
+        for (int id = 0; id < PausedObjects.Count; id++)
+        {
+            PausedObjects[id].UpdatePause();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (IsPause)
+        {
+            return;
+        }
+
+        /*PauseNewManager.PausedObjects.Add(new PauseController());*/
+        for (int id = 0; id < PausedObjects.Count; id++)
+        {
+            PausedObjects[id].FixedUpdatePause();
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (IsPause)
+        {
+            return;
+        }
+
+        for (int id = 0; id < PausedObjects.Count; id++)
+        {
+            PausedObjects[id].LateUpdatePause();
+        }
+    }
+}
