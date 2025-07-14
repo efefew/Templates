85f6f68e1df0e508f8f470f09d8a5ac4ccb4ab70
Роман Сёмин <prodigy19913@mail.ru>
2025-07-14 17:01:32 +0300

    Добавлено метод WaitForPausedSeconds для ожидания паузы на заданное время (#1)
    Выделено объекты в игровой сцене и добавлен метод WaitForPausedSeconds для ожидания паузы на заданное время.
    
    PauseManager.cs изменен, чтобы включить использование UnityEngine, добавить метод WaitForPausedSeconds, и изменить код для выхода из паузы с учётом нового метода.

--- Assets/Project/Game/Scripts/Develop/Architecture/Pause/PauseManager.cs
+++ Assets/Project/Game/Scripts/Develop/Architecture/Pause/PauseManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEngine;
 
 public static class PauseManager
 {
@@ -20,5 +21,30 @@ public static class PauseManager
     {
         IsPause = false;
         OnResume?.Invoke();
+    }
+
+    public static WaitWhile WaitForPausedSeconds(float seconds)
+    {
+        DateTime endTime = DateTime.Now.AddSeconds(seconds);
+        bool isPaused = false;
+        WaitWhile waitPausedTime = new(() =>
+        {
+            DateTime dateTime = DateTime.Now;
+            if (isPaused)
+            {
+                endTime = DateTime.Now.AddSeconds(seconds);
+            }
+            else if (IsPause && dateTime < endTime)
+            {
+                isPaused = true;
+                seconds = (float)(endTime - dateTime).TotalSeconds;
+                return true;
+            }
+
+            if (!IsPause) isPaused = false;
+
+            return IsPause || dateTime < endTime;
+        });
+        return waitPausedTime;
     }
 }
